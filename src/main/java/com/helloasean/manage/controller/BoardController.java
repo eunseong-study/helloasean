aefcf4cfeee2a82ca4ce47de9fdbe5f0275b7e80
bepoz <kangsy763@naver.com>
2020-11-09 16:27:32 +0900

    BoardController methods added

--- src/main/java/com/helloasean/manage/controller/BoardController.java
+++ src/main/java/com/helloasean/manage/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.helloasean.manage.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -13,5 +15,35 @@ public class BoardController {
     public String main(@RequestParam int page, @RequestParam int board_type) {
 
         return "board/board_main";
+    }
+
+    @GetMapping("/read")
+    public String read(@RequestParam int board_type, @RequestParam Long board_id, Model model) {
+
+        return "board/board_read";
+    }
+
+    @GetMapping("/write")
+    public String write(@RequestParam int board_type) {
+
+        return "board/board_write";
+    }
+
+    @PostMapping("/write")
+    public String writePro(@Valid @ModelAttribute("boardDto") BoardDto boardDto, BindingResult result, Model model) {
+
+        return "board/write_success";
+    }
+
+    @GetMapping("/modify")
+    public String modify(@RequestParam int board_type,@RequestParam Long board_id, Model model){
+
+        return "board/board_modify";
+    }
+
+    @PostMapping("modify")
+    public String modifyPro(@Valid @ModelAttribute("boardDto") BoardDto boardDto, BindingResult result, Model model) {
+
+        return "board/modify_success";
     }
 }
