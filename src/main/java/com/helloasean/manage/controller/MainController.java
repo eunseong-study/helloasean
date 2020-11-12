2c7eac53ba3b6d927e14366a6a90c8a86d5d038c
bepoz <kangsy763@naver.com>
2020-11-13 00:24:27 +0900

    application.yml added, MainController @GetMapping added

--- src/main/java/com/helloasean/manage/controller/MainController.java
+++ src/main/java/com/helloasean/manage/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.helloasean.manage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
     public String main(){
         return "index";
     }
