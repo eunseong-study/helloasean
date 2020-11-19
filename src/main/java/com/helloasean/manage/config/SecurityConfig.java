5963f4498a73fdbeac5eb94af9cafe3af2c2a576
bepoz <kangsy763@naver.com>
2020-11-19 15:16:48 +0900

    [ASEAN-8] SecurityConfig modified

--- src/main/java/com/helloasean/manage/config/SecurityConfig.java
+++ src/main/java/com/helloasean/manage/config/SecurityConfig.java
@@ -1,18 +1,36 @@
 package com.helloasean.manage.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+            .and()
+            .formLogin()
+                .loginPage("/user/login")
+            .and()
+            .logout()
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/user/login")
+        ;
     }
 
     @Override
