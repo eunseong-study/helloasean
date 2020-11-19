4aa9d7f4c07f56e418bfe61ab2d524f042951cd6
bepoz <kangsy763@naver.com>
2020-11-19 18:40:13 +0900

    오타수정

--- src/main/java/com/helloasean/manage/repository/FileRepository.java
+++ src/main/java/com/helloasean/manage/repository/FileRepository.java
@@ -3,5 +3,5 @@ package com.helloasean.manage.repository;
 import com.helloasean.manage.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface fileRepository extends JpaRepository<Long, File> {
+public interface FileRepository extends JpaRepository<File, Long> {
 }
